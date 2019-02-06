0b1e084219f6e615104fdeb67d2a7e1d288d1952
rajapandian1803 <47309006+rajapandian1803@users.noreply.github.com>
2019-02-06 14:08:25 +0530

    Update HiveTable.scala

--- HiveTable.scala
+++ HiveTable.scala
@@ -1,4 +1,4 @@
-package com.structure.test
+package com.test
 import org.apache.spark.SparkConf
 import org.apache.spark.api.java.JavaSparkContext
 import org.apache.spark.sql.SQLContext
@@ -7,7 +7,7 @@ import org.apache.spark.sql.SQLContext
 object HiveTable {
 
   def main(args: Array[String]): Unit = {
-    System.setProperty("HADOOP_USER_NAME","nso3prod")
+    //System.setProperty("HADOOP_USER_NAME","nso3prod")
 
 
     //val sparkConf = new SparkConf().setAppName("SparkHive").setMaster("local[*]")
@@ -18,18 +18,10 @@ object HiveTable {
 
     javaSparkCont.hadoopConfiguration.set("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
     javaSparkCont.hadoopConfiguration.set("parquet.enable.summary-metadata", "false")
-
-    /*  val sparkSess: SparkSession = SparkSession.builder()
-        .config("spark.sql.warehouse.dir","hdfs://dayrhegapd016.enterprisenet.org:8020/user/hive/warehouse/")
-        .config("hive.metastore.uris","http://dayrhegapd016.enterprisenet.org:9083")
-       // .master("local[*]")
-        .enableHiveSupport()
-        .getOrCreate()*/
-
-    val datbase = sqlContext.sql("show tables in crp_si_leaflet")
+    val datbase = sqlContext.sql("show tables in <DBNAME>")
     datbase.show()
     datbase.coalesce(1).write.option("header","true")
-      .save("hdfs://dayrhegapd016.enterprisenet.org:8020/user/nso3prod/Test/CategoryDB/")
+      .save("<HDFS PATH>")
   }
 }
 
